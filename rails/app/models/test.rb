3c19f99f282e2ff791a9b878bbf20ca4d6f30ac4
mametaro99 <bearsmichimato99@gmail.com>
2025-01-18 22:53:35 +0900

    adminとtestの関連付けを定義

--- rails/app/models/test.rb
+++ rails/app/models/test.rb
@@ -1,6 +1,7 @@
 class Test < ApplicationRecord
   has_many :questions, dependent: :destroy
   has_many :test_answers, dependent: :destroy
+  belongs_to :admin
 
   validates :title, presence: true
   validates :description, presence: true
