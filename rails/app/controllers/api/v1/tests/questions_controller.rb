4c87f06ce4fb3e1079e2e3d119378e699e52db08
mametaro99 <bearsmichimato99@gmail.com>
2025-01-20 18:32:35 +0900

    questionコントローラーにset_testを追加

--- rails/app/controllers/api/v1/tests/questions_controller.rb
+++ rails/app/controllers/api/v1/tests/questions_controller.rb
@@ -1,9 +1,14 @@
 class Api::V1::Tests::QuestionsController < Api::V1::BaseController
   before_action :set_test
-  before_action :authenticate_admin!, only: [:create, :update, :destroy]
 
   def index
     questions = @test.questions
     render json: questions
+  end
+
+  protected
+
+  def set_test
+    @test = Test.find(params[:test_id])
   end
 end
