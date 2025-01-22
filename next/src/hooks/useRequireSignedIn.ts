a5784b9a321d835417a5c2679404eb479b8dc2fc
mametaro99 <bearsmichimato99@gmail.com>
2025-01-22 17:27:37 +0900

    管理者のログインが必要なページでログインができていない場合に、管理者のサインインページに遷移するメソッドを作成

--- next/src/hooks/useRequireSignedIn.ts
+++ next/src/hooks/useRequireSignedIn.ts
@@ -1,6 +1,6 @@
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
-import { useUserState, useSnackbarState } from '@/hooks/useGlobalState'
+import { useUserState, useSnackbarState, useAdminState } from '@/hooks/useGlobalState'
 
 export function useRequireSignedIn() {
   const router = useRouter()
@@ -17,4 +17,21 @@ export function useRequireSignedIn() {
       router.push('/sign_in')
     }
   }, [user, router, setSnackbar])
+}
+
+export function useRequireAdminSignedIn() {
+  const router = useRouter()
+  const [admin] = useAdminState()
+  const [, setSnackbar] = useSnackbarState()
+
+  useEffect(() => {
+    if (admin.isFetched && !admin.isSignedIn) {
+      setSnackbar({
+        message: 'サインインしてください',
+        severity: 'error',
+        pathname: 'admin/sign_in',
+      })
+      router.push('admin/sign_in')
+    }
+  }, [admin, router, setSnackbar])
 }
