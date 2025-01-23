981630125e42f3f5680bb33f2179d941bc9681f3
mametaro99 <bearsmichimato99@gmail.com>
2025-01-23 20:52:58 +0900

    テストに公開・非公開の状態を追加

--- rails/app/models/test.rb
+++ rails/app/models/test.rb
@@ -5,4 +5,15 @@ class Test < ApplicationRecord
 
   validates :title, presence: true
   validates :description, presence: true
+  enum :status, { unsaved: 10, draft: 20, published: 30 }
+
+  alidate :verify_only_one_unsaved_status_is_allowed
+
+   private
+
+     def verify_only_one_unsaved_status_is_allowed
+       if unsaved? && user.tests.unsaved.present?
+         raise StandardError, "未保存の記事は複数保有できません"
+       end
+     end
 end
