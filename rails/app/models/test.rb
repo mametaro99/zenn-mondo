11981d33e13f06128be42161286b9b63f4e65faf
mametaro99 <bearsmichimato99@gmail.com>
2025-01-25 22:15:11 +0900

    Testのバリデーションを公開時のみに限定

--- rails/app/models/test.rb
+++ rails/app/models/test.rb
@@ -3,17 +3,17 @@ class Test < ApplicationRecord
   has_many :test_answers, dependent: :destroy
   belongs_to :admin
 
-  validates :title, presence: true
-  validates :description, presence: true
+  validates :title, presence: true, if: -> { published? }
+  validates :description, presence: true, if: -> { published? }
   enum :status, { unsaved: 10, draft: 20, published: 30 }
 
   validate :verify_only_one_unsaved_status_is_allowed
 
    private
 
-     def verify_only_one_unsaved_status_is_allowed
-       if unsaved? && admin.tests.unsaved.present?
-         raise StandardError, "未保存の記事は複数保有できません"
-       end
-     end
+    def verify_only_one_unsaved_status_is_allowed
+      if unsaved? && admin.tests.unsaved.present?
+        raise StandardError, "未保存の記事は複数保有できません"
+      end
+    end
 end
