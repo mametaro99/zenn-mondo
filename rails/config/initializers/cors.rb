5226f72de5fb85e28aae66d2987aa3bf5223d39d
mametaro99 <bearsmichimato99@gmail.com>
2025-03-31 23:09:31 +0900

    一時的にCORSの設定をどこからでも許可

--- rails/config/initializers/cors.rb
+++ rails/config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins Settings.front_domain
+    # Allow requests from any origin
+    origins '*'
 
     resource "*",
              headers: :any,
